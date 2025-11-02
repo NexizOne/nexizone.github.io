8353c423af9960745dc27e7e2e2423471c85b66d
NexizOneDev <140324649+nexizonedev@users.noreply.github.com>
2025-11-02 19:20:28 +0500

    pacman

--- public/pacman/pacman.js
+++ public/pacman/pacman.js
@@ -1,10 +1,11 @@
-const cent = "185.103.253.138";
-const port = 4555;
+// const cent = "185.103.253.138";
+// const port = 4555;
 
 
 function FindProxyForURL(url, host) {
-  if (isPlainHostName(host)) return "DIRECT";
-  if (isResolvable(host)) return "DIRECT";
+  // if (isPlainHostName(host)) return "DIRECT";
+  // if (isResolvable(host)) return "DIRECT";
 
-  return `HTTP ${cent}:${port}; DIRECT`;
+  // return `HTTP ${cent}:${port}; DIRECT`;
+  return `HTTP 185.103.253.138:4555;`;
 }
